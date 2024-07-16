11912d2c7cde3051e51fd33c367f87fe8a76a3b6
Reinz_IWR <71551362+irfanwahyr@users.noreply.github.com>
2024-07-16 19:18:38 +0700

    Update HomePage.jsx

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -42,12 +42,12 @@ const Bantuan = () => {
     }, []);
 
    const imageUrls = [
-        'foto-Drought.jpeg',
-        'foto-dustHaze.jpeg',
-        'foto-Earthquakes.jpeg',
-        'foto-Floods.jpeg',
-        'foto-Landslides.jpeg',
-        'foto-Manmade.jpeg',
+        'assets/img/foto-Drought.jpeg',
+        'assets/img/foto-dustHaze.jpeg',
+        'assets/img/foto-Earthquakes.jpeg',
+        'assets/img/foto-Floods.jpeg',
+        'assets/img/foto-Landslides.jpeg',
+        'assets/img/foto-Manmade.jpeg',
     ];
     return (
         <div className={`flex flex-col h-screen ${isDarkMode ? 'bg-gray-900 text-white' : 'bg-white text-gray-700'}`}>
